087859a6bdfa37acd2cdcccbdc18a4ec7aadcfc3
민경준 <wsx8028@gmail.com>
2025-04-04 19:35:06 +0900

    Update 타겟 넘버.java

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,4 +1,32 @@
+import java.util.*;
 class Solution {
+    public int solution(int[] numbers, int target) {
+        
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0,0});
+        int count = 0;
+        while(!q.isEmpty()){
+            int[] val = q.poll();
+            int sum = val[0];
+            int idx = val[1];
+            
+            if(idx == numbers.length){
+                if(sum == target){
+                    count++;
+                    continue;
+                }
+            } else {
+                q.add(new int[]{sum+numbers[idx],idx+1});
+                q.add(new int[]{sum-numbers[idx],idx+1});
+            }
+            
+        }
+        
+        return count;
+        
+    }
+}
+/*
     static int count;
     public int solution(int[] numbers, int target) {
         
@@ -23,3 +51,4 @@ class Solution {
         
     }
 }
+*/
