57b10014b81021bcc804fc04d70863fb9c629b7a
민경준 <wsx8028@gmail.com>
2025-08-18 16:30:49 +0900

    [Silver I] Title: 단지번호붙이기, Time: 100 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,89 +1,63 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
 
-    static int[][] homes;
-    static boolean[][] visited;
-    static int[] x = {1,-1,0,0};
-    static int[] y = {0,0,1,-1};
-    static int cnt;
+    static int[] xx = new int[]{1, -1, 0, 0};
+    static int[] yy = new int[]{0, 0, 1, -1};
 
-
-    public static void main(String arg[]) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        homes = new int[N][N];
-        visited = new boolean[N][N];
-        for(int i = 0; i < N; i++){
-            String[] str = br.readLine().split("");
-            for (int j = 0; j < N; j++){
-                homes[i][j] = Integer.parseInt(str[j]);
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = str.charAt(j) - '0';
             }
         }
-        int result = 0;
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0; i < N; i++){
-            for (int j = 0; j < N; j++){
-                if(homes[i][j] == 1 && !visited[i][j]){
-                    cnt = 0;
-                    dfs(homes,N,i,j);
-//                    bfs(homes,N,i,j);
-                    result++;
-                    list.add(cnt);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        int count = 0;
+        boolean[][] visited = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i][j] == 1 && !visited[i][j]) {
+                    list.add(0);
+                    dfs(n, i, j, arr, list, count, visited);
+                    count++;
                 }
             }
         }
-        System.out.println(result);
-        list.sort((n,m)->n-m);
-        for (Integer i : list) {
-            System.out.println(i);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(count).append("\n");
+        Collections.sort(list);
+        for (int i : list) {
+            sb.append(i).append("\n");
         }
+        System.out.println(sb);
 
     }
-    public static void bfs(int[][] homes, int N, int i, int j){
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{i,j});
+
+    private static void dfs(int n, int i, int j, int[][] arr, ArrayList<Integer> list, int count, boolean[][] visited) {
         visited[i][j] = true;
-        while(!q.isEmpty()){
-            int[] now = q.poll();
-            cnt++;
-            for(int idx = 0; idx < 4; idx++){
-                int a = now[0] + x[idx];
-                int b = now[1] + y[idx];
-                if(a>=0 && b>=0 && a<N && b<N){
-                    if(homes[a][b]==1 && !visited[a][b]){
-                        q.add(new int[]{a,b});
-                        visited[a][b] = true;
+        list.set(count, list.get(count) + 1);
 
-                    }
-                }
-
-            }
-
-        }
-
-    }
-    public static void dfs(int[][] homes, int N, int i, int j){
-        visited[i][j] = true;
-        cnt++;
-
-        for(int idx = 0; idx < 4; idx++){
-            int a = i + x[idx];
-            int b = j + y[idx];
-            if(a>=0 && b>=0 && a<N &&b<N){
-                if(!visited[a][b] && homes[a][b] == 1){
-                    dfs(homes,N,a,b);
+        for (int k = 0; k < 4; k++) {
+            int x = i + xx[k];
+            int y = j + yy[k];
+            if (x >= 0 && x < n && y >= 0 && y < n) {
+                if (arr[x][y] == 1 && !visited[x][y]) {
+                    dfs(n, x, y, arr, list, count, visited);
                 }
             }
-
-
         }
-
     }
 
 
 }
-
-
