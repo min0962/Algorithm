d6220f3958fb09d231df260356f3144e62e506f8
민경준 <wsx8028@gmail.com>
2025-08-29 16:49:42 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 104 ms, Memory: 56856 KB, Score: 92 point -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static long[] memo;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        memo = new long[n + 1];
+
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        System.out.println(fibbo(n - 1) % 1_000_000_007);
+
+    }
+
+    private static long fibbo(int n) {
+
+        if (n <= 1) {
+            return memo[n] = 1;
+        }
+
+        if (memo[n] != 0) {
+            return memo[n];
+        }
+
+        return memo[n] = fibbo(n - 1) + fibbo(n - 2);
+
+    }
+
+}
+
