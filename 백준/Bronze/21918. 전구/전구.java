0117023e31276b5ac7505e0b7497bb4ebb062522
민경준 <wsx8028@gmail.com>
2025-09-17 18:06:34 +0900

    [Bronze II] Title: 전구, Time: 440 ms, Memory: 31036 KB -BaekjoonHub

---
+++ 백준/Bronze/21918. 전구/전구.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] state = new int[n + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            state[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (a == 1) {
+                state[b] = c;
+            } else if (a == 2) {
+                for (int j = b; j <= c; j++) {
+                    state[j] = (state[j] + 1) % 2;
+                }
+            } else if (a == 3) {
+                for (int j = b; j <= c; j++) {
+                    state[j] = 0;
+                }
+            } else {
+                for (int j = b; j <= c; j++) {
+                    state[j] = 1;
+                }
+            }
+
+        }
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(state[i] + " ");
+        }
+
+
+    }
+
+}
+
