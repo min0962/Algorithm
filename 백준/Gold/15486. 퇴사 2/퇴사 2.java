b9bc933bdb50e536d724dc0b7dc53c21f6469d79
민경준 <wsx8028@gmail.com>
2025-12-23 15:42:58 +0900

    [Gold V] Title: 퇴사 2, Time: 676 ms, Memory: 315996 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] t = new int[n + 1];
+        int[] p = new int[n + 1];
+        int[] dp = new int[n + 1];
+        
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            t[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = Math.max(dp[i], dp[i - 1]);
+            
+            int next = i + t[i] - 1;
+            if (next <= n) {
+                dp[next] = Math.max(dp[next], dp[i - 1] + p[i]);
+            }
+        }
+
+        System.out.println(dp[n]);
+
+    }
+
+}
