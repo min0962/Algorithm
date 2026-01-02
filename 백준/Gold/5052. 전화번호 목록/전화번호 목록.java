087e71d44eb8f87b1f7ce7861f495d9e896d6556
민경준 <wsx8028@gmail.com>
2026-01-02 14:13:23 +0900

    [Gold IV] Title: 전화번호 목록, Time: 604 ms, Memory: 34400 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int tc = 0; tc < t; tc++) {
+            int n = Integer.parseInt(br.readLine());
+            String[] arr = new String[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = br.readLine();
+            }
+            Arrays.sort(arr);
+            String yn = "YES";
+
+            for (int i = 0; i < n - 1; i++) {
+                if (arr[i + 1].startsWith(arr[i])) {
+                    yn = "NO";
+                    break;
+                }
+            }
+            System.out.println(yn);
+        }
+
+    }
+
+}
