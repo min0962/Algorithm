d8b2de186fddafe360e9b2b67b6199bb51253467
민경준 <wsx8028@gmail.com>
2026-03-10 14:14:09 +0900

    [Gold V] Title: 애너그램, Time: 488 ms, Memory: 30496 KB -BaekjoonHub

---
+++ 백준/Gold/6443. 애너그램/애너그램.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static StringBuilder sb;
+    static String str;
+    static char[] arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int tc = 0; tc < n; tc++) {
+            str = br.readLine();
+            arr = str.toCharArray();
+            Arrays.sort(arr);
+            sb = new StringBuilder();
+            visited = new boolean[arr.length];
+
+            dfs(0, new char[arr.length]);
+
+            System.out.print(sb);
+        }
+    }
+
+    private static void dfs(int depth, char[] result) {
+
+        if (depth == str.length()) {
+            for(char c : result) {
+                sb.append(c);
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            if(i > 0 && arr[i] == arr[i - 1] && !visited[i - 1]) {
+                continue;
+            }
+            visited[i] = true;
+            result[depth] = arr[i];
+            dfs(depth + 1, result);
+            visited[i] = false;
+        }
+    }
+}
