3a9dc7d62df21c5413ce7dfe05bd673ae9a594ef
민경준 <wsx8028@gmail.com>
2026-03-10 14:14:44 +0900

    [Gold V] Title: 애너그램, Time: 440 ms, Memory: 30544 KB -BaekjoonHub

--- 백준/Gold/6443. 애너그램/애너그램.java
+++ 백준/Gold/6443. 애너그램/애너그램.java
@@ -1,53 +1,55 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
-    static StringBuilder sb;
-    static String str;
-    static char[] arr;
-    static boolean[] visited;
+	static StringBuilder sb;
+	static String str;
+	static char[] arr;
+	static boolean[] visited;
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 
-        for (int tc = 0; tc < n; tc++) {
-            str = br.readLine();
-            arr = str.toCharArray();
-            Arrays.sort(arr);
-            sb = new StringBuilder();
-            visited = new boolean[arr.length];
+		for (int tc = 0; tc < n; tc++) {
+			str = br.readLine();
+			arr = str.toCharArray();
+			Arrays.sort(arr);
+			sb = new StringBuilder();
+			visited = new boolean[arr.length];
 
-            dfs(0, new char[arr.length]);
+			dfs(0, new char[arr.length]);
 
-            System.out.print(sb);
-        }
-    }
+			System.out.print(sb);
+		}
+	}
 
-    private static void dfs(int depth, char[] result) {
+	private static void dfs(int depth, char[] result) {
 
-        if (depth == str.length()) {
-            for(char c : result) {
-                sb.append(c);
-            }
-            sb.append("\n");
-            return;
-        }
+		if (depth == str.length()) {
+			for (char c : result) {
+				sb.append(c);
+			}
+			sb.append("\n");
+			return;
+		}
 
-        for (int i = 0; i < arr.length; i++) {
-            if (visited[i]) {
-                continue;
-            }
-            if(i > 0 && arr[i] == arr[i - 1] && !visited[i - 1]) {
-                continue;
-            }
-            visited[i] = true;
-            result[depth] = arr[i];
-            dfs(depth + 1, result);
-            visited[i] = false;
-        }
-    }
+		for (int i = 0; i < arr.length; i++) {
+			if (visited[i]) {
+				continue;
+			}
+			if (i > 0 && arr[i] == arr[i - 1] && !visited[i - 1]) {
+				continue;
+			}
+			visited[i] = true;
+			result[depth] = arr[i];
+			dfs(depth + 1, result);
+			visited[i] = false;
+		}
+	}
 }
